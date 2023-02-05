37024673c2bea256e4718ceaba2a345a78109e24
Manideep karne <123486719+Manideepkarne@users.noreply.github.com>
2023-02-04 19:38:09 -0600

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+
+a = 5
+for i in range(1,a+1):
+ print("*" * i)
+for j in range(a-1,0,-1):
+ print("*" * j)
+
---
+++ q3.py
@@ -0,0 +1,8 @@
+
+x = [23,'python',23.98]
+n=[]
+length=len(x)
+for i in range(length):
+    n.append(type(x[i]))
+
+print(n)
